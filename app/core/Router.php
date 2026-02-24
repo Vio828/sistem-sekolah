a913f74b57895f0196887165c2f84bb2f1f83d89
Vio828 <azelievio@gmail.com>
2026-02-24 08:30:28 +0700

    wip: setup tailwind and setup views

--- app/core/Router.php
+++ app/core/Router.php
@@ -5,29 +5,49 @@ use App\Controllers\StudentController;
 
 class Router
 {
+    private array $routes = [];
 
-public function run()
-{
-    $method = $_SERVER['REQUEST_METHOD'];
-    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-    if($method == 'GET' && $uri == '/students') {
-        require_once './app/controllers/StudentController.php';
-        $controller = new StudentController();
-        $controller->index();
-        return;
+    public function add(string $method, string $uri, string $controller, string $function)
+    {
+        $this->routes[] = [
+            'method' => $method,
+            'uri' => $uri,
+            'controller' => $controller,
+            'function' => $function,
+        ];
     }
-    
-    if($method == 'GET' && $uri == '/students/create') {
-        require_once './app/controllers/StudentController.php';
-        $controller = new StudentController();
-        $controller->create();
-        return;
-    }
+    public function run()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-    http_response_code(404);
-    echo '<h1>404 - Page Not Found</h1>';
-}
+        foreach ($this->routes as $route) {
+
+            $pattern = str_replace(
+                '{id}',
+                '([0-9]+)',
+                $route['uri']
+            );
+            $pattern = '#^' . $pattern . '$#';
+            // /students/{id} => /students/#^([0-9]+)$# = /students/1
+
+            if (preg_match($pattern, $uri, $matches)) {
+                array_shift($matches);
+
+                require_once './app/controllers/' . $route['controller'] . '.php';
+
+                $controllerClass = 'App\\Controllers\\' . $route['controller'];
+                $controller = new $controllerClass();
+                $function = $route['function'];
+
+                call_user_func_array([$controller, $function], $matches);
+                return;
+            }
+        }
+
+        http_response_code(404);
+        echo '<h1>404 - Page Not Found</h1>';
+    }
 
 }
 ?>
--- public/index.php
+++ public/index.php
@@ -5,5 +5,10 @@ use App\Core\Router;
 
 $router = new Router(); 
 
+//Register Routes
+$router->add('GET', '/students', 'StudentController', 'index');
+$router->add('GET', '/students/create', 'StudentController', 'create');
+$router->add('GET', '/students/{id}', 'StudentController', 'show');
+
 $router->run();
 ?>
